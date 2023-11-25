3930086ae69031d8285c32d470fa97a7f89f20ea
Jonathan Cochran <42702555+ionzzu@users.noreply.github.com>
2023-11-25 14:30:28 -0500

    update constant names

--- ads_helpers.py
+++ ads_helpers.py
@@ -3,8 +3,8 @@ import re
 import pandas as pd
 from sklearn import preprocessing
 
-DATA_PATTERN = r'Timestamp:\s+(\d+\.\d+)\s+ID:\s+(\w+)\s+(\w+)\s+DLC:\s+(\d+)\s+((?:\w{2}\s+)+)'
-NULL_DATA_PATTERN = r'Timestamp:\s+(\d+\.\d+)\s+ID:\s+(\w+)\s+(\w+)\s+DLC:\s+(\d+)'
+RE_DATA_PATTERN = r'Timestamp:\s+(\d+\.\d+)\s+ID:\s+(\w+)\s+(\w+)\s+DLC:\s+(\d+)\s+((?:\w{2}\s+)+)'
+RE_NULL_DATA_PATTERN = r'Timestamp:\s+(\d+\.\d+)\s+ID:\s+(\w+)\s+(\w+)\s+DLC:\s+(\d+)'
 
 
 def txt_to_df(dataset_dict:dict, data_dir:str, dataset:str) -> pd.DataFrame:
@@ -14,7 +14,7 @@ def txt_to_df(dataset_dict:dict, data_dir:str, dataset:str) -> pd.DataFrame:
     data_fields = []
     with open(f'{data_dir}/{dataset_dict[dataset]}', 'r') as f:
         for line in f:
-            re_match = re.match(DATA_PATTERN, line)
+            re_match = re.match(RE_DATA_PATTERN, line)
             if re_match:
                 timestamp, msg_id, _, dlc, data_field = re_match.groups()
                 timestamps.append(float(timestamp))
@@ -22,7 +22,7 @@ def txt_to_df(dataset_dict:dict, data_dir:str, dataset:str) -> pd.DataFrame:
                 dlcs.append(int(dlc))
                 data_fields.append(data_field.strip())
             else:
-                re_match = re.match(NULL_DATA_PATTERN, line)
+                re_match = re.match(RE_NULL_DATA_PATTERN, line)
                 if re_match:
                     timestamp, msg_id, _, dlc = re_match.groups()
                     timestamps.append(float(timestamp))
