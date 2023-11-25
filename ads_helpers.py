6879173ea639a49a603515e7c56e1a7b6add7fb4
Jonathan Cochran <42702555+ionzzu@users.noreply.github.com>
2023-11-25 14:29:38 -0500

    add re pattern as constants

--- ads_helpers.py
+++ ads_helpers.py
@@ -3,18 +3,18 @@ import re
 import pandas as pd
 from sklearn import preprocessing
 
+DATA_PATTERN = r'Timestamp:\s+(\d+\.\d+)\s+ID:\s+(\w+)\s+(\w+)\s+DLC:\s+(\d+)\s+((?:\w{2}\s+)+)'
+NULL_DATA_PATTERN = r'Timestamp:\s+(\d+\.\d+)\s+ID:\s+(\w+)\s+(\w+)\s+DLC:\s+(\d+)'
+
 
 def txt_to_df(dataset_dict:dict, data_dir:str, dataset:str) -> pd.DataFrame:
-    re_pattern = r'Timestamp:\s+(\d+\.\d+)\s+ID:\s+(\w+)\s+(\w+)\s+DLC:\s+(\d+)\s+((?:\w{2}\s+)+)'
-    null_pattern = r'Timestamp:\s+(\d+\.\d+)\s+ID:\s+(\w+)\s+(\w+)\s+DLC:\s+(\d+)'
-
     timestamps = []
     ids = []
     dlcs = []
     data_fields = []
     with open(f'{data_dir}/{dataset_dict[dataset]}', 'r') as f:
         for line in f:
-            re_match = re.match(re_pattern, line)
+            re_match = re.match(DATA_PATTERN, line)
             if re_match:
                 timestamp, msg_id, _, dlc, data_field = re_match.groups()
                 timestamps.append(float(timestamp))
@@ -22,7 +22,7 @@ def txt_to_df(dataset_dict:dict, data_dir:str, dataset:str) -> pd.DataFrame:
                 dlcs.append(int(dlc))
                 data_fields.append(data_field.strip())
             else:
-                re_match = re.match(null_pattern, line)
+                re_match = re.match(NULL_DATA_PATTERN, line)
                 if re_match:
                     timestamp, msg_id, _, dlc = re_match.groups()
                     timestamps.append(float(timestamp))
